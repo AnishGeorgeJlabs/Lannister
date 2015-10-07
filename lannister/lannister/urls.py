a4c8bc6cec6cc65cb8750bb5a6eeaec160743b8c
AnishGeorgeJlabs <anish.george@jlabs.co>
2015-10-07 15:32:52 +0530

    simple

--- lannister/lannister/urls.py
+++ lannister/lannister/urls.py
@@ -9,6 +9,8 @@ Function views
 Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+
+    Modifications
 Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
