c8a5cd8efa609cef188ce637d24163e6b1c56c12
AnishGeorgeJlabs <anish.george@jlabs.co>
2015-10-07 15:08:48 +0530

    Finally setup the deployment scripts to run perfectly

--- lannister/lannister/settings.py
+++ lannister/lannister/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'corsheaders',
     'tyrion',
+    'jaime'
 )
 
 MIDDLEWARE_CLASSES = (
--- lannister/lannister/urls.py
+++ lannister/lannister/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     # url(r'^admin/', include(admin.site.urls)),
-    url(r'^tyrion/', include('tyrion.urls'))
+    url(r'^tyrion/', include('tyrion.urls')),
+    url(r'^jaime/', include('jaime.urls'))
 ]
